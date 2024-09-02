58ca00ed20fe1f06f4c787e3c2e49252725c62fe
taskscheduler-app <a@a>
2024-09-02 21:18:05 +0100

    PWA config

---
+++ config/serviceWorker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'task-scheduler-cache-v1';
+const urlsToCache = ['/', '/index.html', '/bundle.js'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheAllowlist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
